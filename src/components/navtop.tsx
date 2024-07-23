66a9c65423d88824f2bacf4ce2378891945302da
abuzaid911 <sabobee911@gmail.com>
2024-07-23 13:00:26 +0300

    adding border to the image

--- src/components/navtop.tsx
+++ src/components/navtop.tsx
@@ -43,8 +43,8 @@ function NavTop() {
             </>)}
             {session.status == "authenticated" && session.data.user?.image && <div className="indicator">
               <div className="dropdown dropdown-end">
-                <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-                  <div className="w-10 rounded-full">
+                <label tabIndex={0} className="btn btn-ghost btn-circle avatar ">
+                  <div className="w-10 rounded-full border-accent">
                     <img src={session.data.user?.image} />
                   </div>
                 </label>
