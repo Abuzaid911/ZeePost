b7cc6d6df88333ae70e0ab2e46892c13cb74e325
abuzaid911 <sabobee911@gmail.com>
2024-07-23 12:48:15 +0300

    min post length 1 or more

--- src/server/trpc/router/example.ts
+++ src/server/trpc/router/example.ts
@@ -8,7 +8,7 @@ export const exampleRouter = router({
     .input(
       z.object({
         title: z.string().min(1),
-        content: z.string().min(3),
+        content: z.string().min(1),
       })
     )
     .mutation(async ({ input, ctx }) => {
